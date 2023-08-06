e9d465b4bc5da803886511bb3b0802de394ec67a
aleksandr.stolbov <aleksandr.stolbov@libertexgroup.com>
2023-08-06 13:16:11 +0200

    analyse function implementation and test cases

--- heatmap.go
+++ heatmap.go
@@ -5,6 +5,7 @@ import (
 	"github.com/llgcode/draw2d/draw2dimg"
 	"image"
 	"image/color"
+	"math/bits"
 )
 
 type Heatmap struct {
@@ -48,20 +49,37 @@ func (hm Heatmap) Draw(data [][]int) error {
 }
 
 // analyse returns width, height of the given matrix and max and min values
-// utilizing the function to get useful data in single interation
+// utilizing the function to get useful data in single interation O(x*y)
 // TODO: add test
 func analyse(d [][]int) (width int, height int, max int, min int) {
+	max = (1 << bits.UintSize) / -2 // set minimal int value
+	min = (1<<bits.UintSize)/2 - 1  // set maximal int value
+
 	for _, row := range d {
-		width = 0
+		x := 0
 		for _, i := range row {
-			fmt.Printf("%d", i)
-			width = width + 1
+			x = x + 1
 			if i > max {
 				max = i
 			}
+			if i < min {
+				min = i
+			}
 		}
+
+		if x >= width {
+			width = x
+		} else {
+			min = 0 // if horizontal row has fewer items, that means min value is 0 TODO: proper handle negative values
+		}
+
 		height = height + 1
-		fmt.Printf("\n")
+	}
+
+	// TODO: proper handle negative values #2 - this is not uint
+	if width == 0 {
+		max = 0
+		min = 0
 	}
 
 	return width, height, max, min
--- heatmap_test.go
+++ heatmap_test.go
@@ -1,18 +1,111 @@
 package heatmap
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestAnalyse(t *testing.T) {
-	data := [][]int{
-		{1, 3, 5, 7},
-		{0, 1, 2, 5},
-		{5, 3, 2, 1},
+	tests := []struct {
+		Data              [][]int
+		WantHorizontalLen int
+		WantVerticalLen   int
+		WantMax           int
+		WantMin           int
+	}{
+		// case 1
+		{
+			Data: [][]int{
+				{1, 3, 5, 7},
+				{1, 1, 2, 5},
+				{5, 3, 2, 1},
+			},
+			WantHorizontalLen: 4,
+			WantVerticalLen:   3,
+			WantMax:           7,
+			WantMin:           1,
+		},
+
+		// case 2
+		{
+			Data: [][]int{
+				{1, 2, 3},
+				{0, 1, 2},
+				{50, 3, 2},
+			},
+			WantHorizontalLen: 3,
+			WantVerticalLen:   3,
+			WantMax:           50,
+			WantMin:           0,
+		},
+
+		// case 3
+		{
+			Data: [][]int{
+				{},
+				{},
+			},
+			WantHorizontalLen: 0,
+			WantVerticalLen:   2,
+			WantMax:           0,
+			WantMin:           0,
+		},
+
+		// case 4
+		{
+			Data: [][]int{
+				{1, 2, 3},
+				{1},
+			},
+			WantHorizontalLen: 3,
+			WantVerticalLen:   2,
+			WantMax:           3,
+			WantMin:           0,
+		},
+
+		// case 5
+		{
+			Data: [][]int{
+				{1, 2},
+				{},
+			},
+			WantHorizontalLen: 2,
+			WantVerticalLen:   2,
+			WantMax:           2,
+			WantMin:           0,
+		},
+
+		// case 5
+		{
+			Data: [][]int{
+				{1, 2},
+			},
+			WantHorizontalLen: 2,
+			WantVerticalLen:   1,
+			WantMax:           2,
+			WantMin:           1,
+		},
 	}
 
-	w, h, max, min := analyse(data)
+	for k, tt := range tests {
+		name := fmt.Sprintf("case %d", k+1)
+		t.Run(name, func(t *testing.T) {
+			h, v, max, min := analyse(tt.Data)
 
-	if w != 4 && h != 3 && max != 7 && min != 0 {
-		t.Fail()
+			if h != tt.WantHorizontalLen {
+				t.Errorf("Horizontal Len error: got %d, want %d", h, tt.WantHorizontalLen)
+			}
+			if v != tt.WantVerticalLen {
+				t.Errorf("Vertical Len error: got %d, want %d", h, tt.WantVerticalLen)
+			}
+			if max != tt.WantMax {
+				t.Errorf("Max value error: got %d, want %d", max, tt.WantMax)
+			}
+			if min != tt.WantMin {
+				t.Errorf("Min value error: got %d, want %d", min, tt.WantMin)
+			}
+
+		})
 	}
 
 }
